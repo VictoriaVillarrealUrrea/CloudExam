5207a747668f74290197959b8aa0d8580b4bc6f0
Ana Victoria Villarreal <vickypop95@gmail.com>
2021-03-18 15:59:49 -0600

    Hi, just checking.

--- frontend/server.js
+++ frontend/server.js
@@ -1,11 +1,13 @@
 //Creamos ruta.
+const { response } = require('express');
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 8080; //MISMA DE LAS PRACTICAS
 url = "https://toneanalyzer-victoriavillarrealnew-rested-bushbuck-lf.mybluemix.net/analyze"; // URL DE CF /ANALYZE es la ruta creada
-
-let container = document.querySelector('#container'); //Practica3
 app.use(express.json);
+let container = document.getElementById('#container'); //Practica3
+//Document is not
+//window.addEventListener('container', getPostResult, false);
 
 //SERVER
 const server = app.listen(port, () => {
@@ -18,6 +20,30 @@ app.get('/', (req, res) => {
 });
 
 //EVENT LISTENERS
-container.addEventListener('Success', (a) => ({ //Nombre del boton
+container.addEventListener('Success', function(a) { //Nombre del boton practica 3
+    let br = ["bad request"];
+    let options = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            text: container.text.value //exampleFormControlTextarea1 cambio de nombre
+        })
+    }
+    data = GetResponse(url, options);
+    console.log(data);
+    if (data.status == 200) {
+        result = data.result;
+        div = `<div><p>${JSON.stringify(result.document_tone)}</p><p>${JSON.stringify(result.sentences_tone)}</p></div>`;
+        console.log(div)
+        document.querySelector('#Success').innerHTML = div;
+    } else {
+        res.status(400).send(br);
+    }
+});
 
-}));
+async function GetResponse(url, options) {
+    res = await fetch(url, options);
+    return await res.json();
+};
