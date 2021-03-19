76f114b4f2ecf16602a6e5d592513a60145f2f27
Ana Victoria Villarreal <vickypop95@gmail.com>
2021-03-18 19:42:49 -0600

    Seguimos con el error re CORS

--- frontend/index.js
+++ frontend/index.js
@@ -7,13 +7,13 @@ url = "https://toneanalyzer-victoriavillarrealnew-rested-bushbuck-lf.mybluemix.n
 //Document is not
 boton = document.getElementById("successbtn");
 container = document.getElementById("container");
-
 console.log(boton);
 //window.addEventListener('container', getPostResult, false);
 //EVENT LISTENERS
 boton.addEventListener('click', function(a) { //Nombre del boton practica 3
-    console.log("Aqui 1 entrando")
-    let br = ["bad request"];
+
+    GetResponse();
+
     /*let options = {
         method: 'POST',
         headers: {
@@ -26,13 +26,34 @@ boton.addEventListener('click', function(a) { //Nombre del boton practica 3
         })
     };*/
 
+
+});
+
+async function GetResponse() {
+    console.log("Aqui 1 entrando")
+    let br = ["bad request"];
+    console.log("Aqui 1 entrando al get responses")
+    texto = document.getElementById("textbox").value;
+
+    console.log(texto.value)
+    res = await fetch(url, {
+        method: 'POST', // *GET, POST, PUT, DELETE, etc.
+        //mode: 'no-cors', // no-cors, *cors, same-origin
+        headers: {
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': '*'
+                // 'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        body: JSON.stringify({ text: texto }) // body data type must match "Content-Type" header
+    });
+    console.log("RESSSSS");
+    console.log(res);
     console.log("Aqui 1 getresponses")
-    data = GetResponse();
-    console.log("DATAAAAAAAAAAAAAA")
+    data = res.json()
+    console.log("RESssssssssssssssssSSSS");
     console.log(data);
-    console.log(data.status);
     if (data.status == 200) {
-        result = data.result;
+        result = res.result;
         div = `<div><p>${JSON.stringify(result.document_tone)}</p><p>${JSON.stringify(result.sentences_tone)}</p></div>`;
         console.log(div)
         document.getElementById('Success').innerHTML = div;
@@ -40,22 +61,4 @@ boton.addEventListener('click', function(a) { //Nombre del boton practica 3
         console.log(br)
             //res.status(400).send(br);
     }
-});
-
-async function GetResponse() {
-    console.log("Aqui 1 entrando al get responses")
-    texto = document.getElementById("textbox");
-    console.log(texto.value)
-    res = await fetch(url, {
-        method: 'POST', // *GET, POST, PUT, DELETE, etc.
-        mode: 'no-cors', // no-cors, *cors, same-origin
-        headers: {
-            'Content-Type': 'application/json'
-                // 'Content-Type': 'application/x-www-form-urlencoded',
-        },
-        body: JSON.stringify({ text: texto.value }) // body data type must match "Content-Type" header
-    });
-    console.log("RESSSSS");
-    console.log(res);
-    return res.json(); // parses JSON response into native JavaScript objects
 };
